33f4f844f4212a5516f37eae7432a023fc019344
Antonio06Martins <toni6martins@gmail.com>
2021-05-25 00:02:40 -0300

    first commit api-micronaut-rest

---
+++ src/main/kotlin/com/antonio/Controller/HelloController.kt
@@ -0,0 +1,16 @@
+package com.antonio.Controller
+
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.RequestAttribute
+
+@Controller("/hello")
+class HelloController {
+
+    @Get(consumes = [MediaType.TEXT_PLAIN])
+    fun hello(): String {
+        return "Hello World!"
+    }
+}
